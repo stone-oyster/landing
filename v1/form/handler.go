0e66503db0847446e3a7cad11447225199f3c08c
Adriano <adriano.selaviles@gmail.com>
2020-01-26 19:15:30 -0800

    form catcher

--- v1/form/handler.go
+++ v1/form/handler.go
@@ -1,6 +1,7 @@
 package form
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -19,13 +20,23 @@ func NewFormService() *Service {
 		Router: mux.NewRouter(),
 	}
 
-	svc.Router.Methods(http.MethodGet).Path("/submit").HandlerFunc(formHandler)
+	svc.Router.Methods(http.MethodPost).Path("/submit").HandlerFunc(formHandler)
 
 	return svc
 }
 
 func formHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
+	}
+
+	name := r.Form["name"]
+	email := r.Form["email"]
+	message := r.Form["message"]
+
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("your details were submitted!"))
+	w.Write([]byte(fmt.Sprintf("name: %s, email: %s, message: %s", name, email, message)))
 	return
 }
